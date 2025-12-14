ea3fbd78feedf9193e30b32f694d249e6ba80b21
Yuvan Chikka <147429542+Ultraviolet-Chikorita@users.noreply.github.com>
2025-12-14 16:40:05 +0000

    Add new URL patterns for farcaster and webhook

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('process-name/', views.processName, name='process_name'),
+    path('.well-known/farcaster.json', views.farcaster_manifest_view, name='farcaster-manifest'),
+    path('api/webhook', views.webhook_view, name='webhook'),
 ]
